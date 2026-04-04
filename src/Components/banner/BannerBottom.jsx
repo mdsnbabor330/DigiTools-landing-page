de7262cb87dd2599c8087031f6a5f7664cb187e5
Mohammad Nahid Uddin Babor <mdsnbabor828@gmail.com>
2026-04-04 23:40:25 +0600

    added metrics banner

--- src/Components/banner/BannerBottom.jsx
+++ src/Components/banner/BannerBottom.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const BannerBottom = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="p-20 bg-linear-to-r from-[#4F39F6] to-[#9514FA]">
+      <div className="max-mx text-center flex flex-col md:flex-row justify-center items-center gap-10 md:gap-35 text-white">
+        <div className="md:px-15">
+          <h2 className="text-[60px] font-extrabold">50k+</h2>
+          <p className="text-[24px]">Active Users</p>
         </div>
-    );
+        <div className="md:px-15 md:border-l-2 md:border-r-2 border-0">
+          <h2 className="text-[60px] font-extrabold">200+</h2>
+          <p className="text-[24px]">Premium Tools</p>
+        </div>
+        <div className="md:px-15">
+          <h2 className="text-[60px] font-extrabold">4.9</h2>
+          <p className="text-[24px]">Rating</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BannerBottom;
